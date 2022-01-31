6a0d775f24fdc6424e56184fc01e7f067dfa3f20
ariel <arielbenzikri@gmail.com>
2022-01-31 21:09:34 +0200

    committing new files

---
+++ tests/e2e.py
@@ -0,0 +1,13 @@
+
+
+def test_scores_service():
+    """it’s purpose is to test our web service. It will get the application
+    URL as an input, open a browser to that URL, select the score element in our web page,
+    check that it is a number between 1 to 1000 and return a boolean value if it’s true or not."""
+    pass
+
+
+def main_function():
+    """call our tests function. The main function will return -1 as an OS exit
+    code if the tests failed and 0 if they passed."""
+    pass
