6eb813478df739fa450c74420def40a2fb66aec8
ariel <95013498+matbuha@users.noreply.github.com>
2022-02-24 21:03:27 +0200

    Update MainScores.py

--- MainScores.py
+++ MainScores.py
@@ -17,7 +17,7 @@ def my_flask():
         return render_template('about.html', title='About')
 
     if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=5001)
+        app.run(debug=True, host='0.0.0.0', port=8777)
 
 
 my_flask()
